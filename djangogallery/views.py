15d63f8e5611259a68000971780de7dd1be3aad6
prateekchandra <prateekchandra1027@gmail.com>
2020-12-17 22:06:52 +0530

    Added Django-Gallery

---
+++ djangogallery/views.py
@@ -0,0 +1,36 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from myapp.models import Image, Category
+
+def this_is_about(request): 
+    print("This is about page request")
+    return render(request, "about.html")
+
+
+def show_home_page(request):
+
+    images = Image.objects.all()
+    cat = Category.objects.all()
+    data = {'images': images, 'cat': cat} 
+    print("This is Home page request")
+    return render(request, "home.html",data)
+    
+
+
+
+
+def show_category_page(request, cid):
+    
+
+    print(cid)
+    cat = Category.objects.all()
+
+
+    category = Category.objects.get(pk=cid)
+
+    images = Image.objects.filter(cat=category)
+    
+    data = {'images': images, 'cat': cat} 
+    print("This is Home page request")
+    return render(request, "home.html",data)
+    
